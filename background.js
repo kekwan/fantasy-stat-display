a11e0ca217290233a455b60ac8edfe15b5183580
Kenny Kwan <kkwan@tslologic.com>
2018-11-01 15:39:26 -0700

    Refactor to not need persistent background script

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
-sendURL = function (page) {
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.contextMenus.create({
+		"id": "fantasyTracker",
+		"title": "Track This Team",
+	});
+});
+
+chrome.webNavigation.onCompleted.addListener(function() {
+	console.log("This is my favorite website!");
+}, {url: [{urlMatches : 'https://basketball.fantasysports.yahoo.com/nba*'}]});
+
+chrome.contextMenus.onClicked.addListener(function (page) {
 	var url = page.pageUrl;
 	chrome.storage.sync.set({'trackingURL' : url}, function() {
-		console.log('URL setting saved');
 	});
-};
-
-chrome.contextMenus.create({
-  title: "Track This Team's Stats!",
-  contexts:["page"],
-  onclick: sendURL
 });
+
+
