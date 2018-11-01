88fe8d148f60ea66e9593ced96f5e7684490daf9
Kenny Kwan <kkwan@tslologic.com>
2018-11-01 16:42:00 -0700

    remove unneeded permission

--- background.js
+++ background.js
@@ -5,20 +5,14 @@ chrome.runtime.onInstalled.addListener(function () {
 	});
 });
 
-chrome.webNavigation.onCompleted.addListener(function () {
-	console.log("This is my favorite website!");
-}, { url: [{ urlMatches: 'https://basketball.fantasysports.yahoo.com/nba*' }] });
-
 chrome.contextMenus.onClicked.addListener(function (page) {
 	var url = page.pageUrl;
-
 	if (/^https:\/\/basketball\.fantasysports\.yahoo\.com\/nba\/\d+\/\d+$/i.test(url)) {
 		alert("Success! This team will be tracked.")
 		chrome.storage.sync.set({ 'trackingURL': url }, function () {
 		});
 
 	} else {
-
 			alert("Fantasy Basketball Tracker only tracks Yahoo Fantasy Basketball sports team.");
 	}
 });
