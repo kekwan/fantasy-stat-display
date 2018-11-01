3f76506ad814bce0239c40e68294a241a68923a8
Kenny Kwan <kkwan@tslologic.com>
2018-11-01 16:10:18 -0700

    tracks only yahoo teams

--- background.js
+++ background.js
@@ -1,18 +1,26 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
 	chrome.contextMenus.create({
 		"id": "fantasyTracker",
 		"title": "Track This Team",
 	});
 });
 
-chrome.webNavigation.onCompleted.addListener(function() {
+chrome.webNavigation.onCompleted.addListener(function () {
 	console.log("This is my favorite website!");
-}, {url: [{urlMatches : 'https://basketball.fantasysports.yahoo.com/nba*'}]});
+}, { url: [{ urlMatches: 'https://basketball.fantasysports.yahoo.com/nba*' }] });
 
 chrome.contextMenus.onClicked.addListener(function (page) {
 	var url = page.pageUrl;
-	chrome.storage.sync.set({'trackingURL' : url}, function() {
-	});
+
+	if (/^https:\/\/basketball\.fantasysports\.yahoo\.com\/nba\/\d+\/\d+$/i.test(url)) {
+		alert("Success! This team will be tracked.")
+		chrome.storage.sync.set({ 'trackingURL': url }, function () {
+		});
+
+	} else {
+
+			alert("Fantasy Basketball Tracker only tracks Yahoo Fantasy Basketball sports team.");
+	}
 });
 
 
